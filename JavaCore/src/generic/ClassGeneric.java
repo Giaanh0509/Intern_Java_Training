fe3ab936e27e7c28ab3c2f7c82e5d81fb256232b
Giaanh0509 <ngotthe0509.com>
2024-07-23 23:38:34 +0700

    add generic example

---
+++ JavaCore/src/generic/ClassGeneric.java
@@ -0,0 +1,24 @@
+package generic;
+
+public class ClassGeneric<T> {
+    private T t;
+
+    public void set(T t) {
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }
+
+    public static void main(String[] args) {
+        ClassGeneric<Integer> integerClassGeneric = new ClassGeneric<>();
+        ClassGeneric<String> stringClassGeneric = new ClassGeneric<>();
+
+        integerClassGeneric.set(10);
+        stringClassGeneric.set("Hello");
+
+        System.out.println("Integer Value: " + integerClassGeneric.get());
+        System.out.println("String Value: " + stringClassGeneric.get());
+    }
+}
---
+++ JavaCore/src/generic/MethodGeneric.java
@@ -0,0 +1,21 @@
+package generic;
+
+public class MethodGeneric {
+        public static <T> void printArray(T[] inputArray) {
+            for (T element : inputArray) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+
+        public static void main(String[] args) {
+            Integer[] intArray = {1, 2, 3, 4, 5};
+            Character[] charArray = {'A', 'B', 'C', 'D', 'E'};
+
+            System.out.println("Array of Integers:");
+            printArray(intArray);
+
+            System.out.println("Array of Characters:");
+            printArray(charArray);
+        }
+}
