9baba474c8ac412ab615fe50e2f64fcf47ef3dd5
Giaanh0509 <ngotthe0509@gmail.com>
2024-08-07 09:44:58 +0700

    update annotation

--- SpringCore/src/main/java/com/example/springcore/transation/annotation/dao/UserRepository.java
+++ SpringCore/src/main/java/com/example/springcore/transation/annotation/dao/UserRepository.java
@@ -1,7 +1,9 @@
 package com.example.springcore.transation.annotation.dao;
 import com.example.springcore.transation.annotation.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
--- SpringCore/src/main/java/com/example/springcore/transation/annotation/service/MySqlUserService.java
+++ SpringCore/src/main/java/com/example/springcore/transation/annotation/service/MySqlUserService.java
@@ -3,9 +3,11 @@ import com.example.springcore.transation.annotation.dao.UserRepository;
 import com.example.springcore.transation.annotation.entity.User;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class MySqlUserService implements UserService {
 
     private UserRepository userRepository;
